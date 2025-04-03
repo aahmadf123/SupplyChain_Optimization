15c85c0a59793a6e512d08375c57e7b68cfdf52f
aahmadf123 <firas.azfar@gmail.com>
2025-04-03 16:39:48 -0400

    Try to get the UI component setup

---
+++ DemandForecastingApp/UI/ForecastDetailsControl.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows.Controls;
+
+namespace DemandForecastingApp.UI
+{
+    /// <summary>
+    /// Interaction logic for ForecastDetailsControl.xaml
+    /// </summary>
+    public partial class ForecastDetailsControl : UserControl
+    {
+        public ForecastDetailsControl()
+        {
+            InitializeComponent();
+            // TODO: Bind ForecastDataGrid to your forecast details data source.
+        }
+    }
+}
---
+++ DemandForecastingApp/UI/InventoryRecommendationsControl.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows.Controls;
+
+namespace DemandForecastingApp.UI
+{
+    /// <summary>
+    /// Interaction logic for InventoryRecommendationsControl.xaml
+    /// </summary>
+    public partial class InventoryRecommendationsControl : UserControl
+    {
+        public InventoryRecommendationsControl()
+        {
+            InitializeComponent();
+            // TODO: Bind RecommendationsListView to your inventory recommendations data source.
+        }
+    }
+}
