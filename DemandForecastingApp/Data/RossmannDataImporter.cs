68bbd69f9f66799b49b0e1f7b3e03997a0efd16c
aahmadf123 <firas.azfar@gmail.com>
2025-04-03 20:58:14 -0400

    Refactor NuGet configuration and update package references; enhance RossmannSalesRecord feature vector for improved ML model training

--- DemandForecastingApp/Data/RossmannDataImporter.cs
+++ DemandForecastingApp/Data/RossmannDataImporter.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Globalization;
 using DemandForecastingApp.Models;
 using DemandForecastingApp.Utils;
+using DemandForecastingApp.Services;
 
 namespace DemandForecastingApp.Data
 {
