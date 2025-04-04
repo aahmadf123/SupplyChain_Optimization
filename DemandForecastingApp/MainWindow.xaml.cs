eab1298768cf2587d1937f7e91649ba9cfd09fdf
aahmadf123 <firas.azfar@gmail.com>
2025-04-03 20:45:15 -0400

    Remove unused BAML files and clean up generated assembly info for Demand Forecasting App

--- DemandForecastingApp/MainWindow.xaml.cs
+++ DemandForecastingApp/MainWindow.xaml.cs
@@ -3,9 +3,6 @@ using DemandForecastingApp.ViewModels;
 
 namespace DemandForecastingApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
